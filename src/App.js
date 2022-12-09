c6a0b4fc421b0c367030bc00a1c3c490d4d92f53
Pedrohero13 <pedrohero13@gmail.com>
2022-12-08 18:54:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [imageAsFile, setImageAsFile] = useState('')
+  const [imageName, setImageName] = useState('')
+  const [imageClass, setImageClass] = useState('')
+
+  const [imageUrl, setImageUrl] = useState('')
+  const handleImageAsFile = (e) => {
+       const image = e.target.files[0]
+        setImageAsFile(imageFile => (image))
+  }
+
+  async function handleSubmit() {
+  
+
+  const formParams = new FormData();
+  formParams.append('file', imageAsFile)
+
+    const response = await fetch("http://localhost:9000", {
+      
+      method: 'POST',
+      headers: {
+        
+        'Access-Control-Allow-Origin' : '*'
+      },
+      body: formParams
+    });
+    const data = await response.json();
+    console.log(data)
+    setImageName(data.data.name_image);
+    setImageUrl(data.data.url_image);
+    setImageClass(data.data.class);
+  
+}
+
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form class="form-horizontal" >  
+      <div class="input-group">
+        <input onChange={handleImageAsFile} type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload"/>
+        <button onClick={handleSubmit} class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04">Enviar</button>
+        </div>
+        </form>
+        {imageUrl ? (
+          <div class="card mb-3">
+          <img src={ imageUrl} class="card-img-top" alt="Foto mascota" style={{maxHeight:"400px", maxWidth:"600px"}}/>
+        <div class="card-body">
+              <h3 class="card-title">Nombre: {imageName}</h3>
+              <h5 class="card-title">Clase: { imageClass}</h5>
+          <p class="card-text">Imagen detectada con RESTNET API</p>
+          
+        </div>
+</div>
+        ):(<div>No se a seleccionado la imagen</div>)}
+      
       </header>
     </div>
   );
